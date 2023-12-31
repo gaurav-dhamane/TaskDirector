6781c60a7fe927ad8aac857d9bc754a941df0549
Gaurav Dhamane <75262183+gaurav-dhamane@users.noreply.github.com>
2023-12-31 20:44:51 +0530

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    // 'https://url.com'
+    'https://taskdirector.onrender.com',
+    'https://www.taskdirector.onrender.com/
 ]
 
 module.exports = allowedOrigins;
